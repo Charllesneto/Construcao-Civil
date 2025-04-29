c7090b4cb30618a8ddab24c2ecddf5c5fdc919b2
Nodival Neto <netococ6@gmail.com>
2025-04-29 19:54:20 -0300

    Criei métodos
    Criei métodos Get and Setters, importei a biblioteca de Data, escrevi o Override.

--- construcaoCivil/src/model/Etapa.java
+++ construcaoCivil/src/model/Etapa.java
@@ -4,6 +4,9 @@ package model;
  *
  * @author Nodival Neto
  */
+
+import java.time.LocalDate;
+
 public class Etapa {
 
     private int idEtapa;
@@ -18,6 +21,52 @@ public class Etapa {
         this.status = status;
         this.dataInicio = dataInicio;
         this.dataFimPrevista = dataFimPrevista;
+    }
+
+    public int getIdEtapa() {
+        return idEtapa;
+    }
+
+    public void setIdEtapa(int idEtapa) {
+        this.idEtapa = idEtapa;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFimPrevista() {
+        return dataFimPrevista;
+    }
+
+    public void setDataFimPrevista(LocalDate dataFimPrevista) {
+        this.dataFimPrevista = dataFimPrevista;
+    }
+
+    @Override
+    public String toString() {
+        return "Etapa [idEtapa=" + idEtapa + ", descricao=" + descricao + ", status=" + status
+                + ", dataInicio=" + dataInicio + ",dataFimPrevista=" + dataFimPrevista + "]";
     }
 
 }
