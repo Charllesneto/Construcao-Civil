7a54ce21fed4fa531f4acabc1e8c08c784c1f3d3
Nodival Neto <netococ6@gmail.com>
2025-04-29 19:49:25 -0300

    Criado método construtor

--- construcaoCivil/src/model/Etapa.java
+++ construcaoCivil/src/model/Etapa.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 /**
  *
- * @author aluno
+ * @author Nodival Neto
  */
 public class Etapa {
-    
+
+    private int idEtapa;
+    private String descricao;
+    private String status;
+    private LocalDate dataInicio;
+    private LocalDate dataFimPrevista;
+
+    public Etapa(int idEtapa, String descricao, String status, LocalDate dataInicio, LocalDate dataFimPrevista) {
+        this.idEtapa = idEtapa;
+        this.descricao = descricao;
+        this.status = status;
+        this.dataInicio = dataInicio;
+        this.dataFimPrevista = dataFimPrevista;
+    }
+
 }
