0417769dad1b09107980b133ed614e0b11129591
DavidCrr <david.contato62@gmail.com>
2025-05-07 22:41:22 -0300

    adicionei descrição e criei o método para listar os clientes

--- construcaoCivil/src/controller/ClienteController.java
+++ construcaoCivil/src/controller/ClienteController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 public class ClienteController {
     
+// metodo para adicionar clientes 
 public void adicionarCliente(Cliente cliente){
     String sql = "INSERT INTO cliente (id_cliente, nome, tipo_cliente, cpf_cnpj, telefone) VALUES (?, ?, ?, ?, ?)";
     try (Connection conn = Conexao.conectar();
@@ -31,4 +32,31 @@ public void adicionarCliente(Cliente cliente){
             System.err.println("Erro ao cadastrar cliente: " + e.getMessage());
         }
     }
+// Lista todos os clientes, método retorna uma lista com os dados de clientes vindos do BD.
+public List<Cliente> listarClientes() {
+        List<Cliente> lista = new ArrayList<>();
+        String sql = "SELECT * FROM cliente";
+
+        try (Connection conn = Conexao.conectar();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Cliente cliente = new Cliente(
+                    rs.getInt("id_cliente"),
+                    rs.getString("nome"),
+                    rs.getString("tipo_cliente"),
+                    rs.getString("cpf_cnpj"),
+                    rs.getString("telefone")
+                );
+                lista.add(cliente);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao listar clientes: " + e.getMessage());
+        }
+        return lista;
+    }
+
+
 }
