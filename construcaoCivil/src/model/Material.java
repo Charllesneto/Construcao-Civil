23bb158292c89aa49bdda8216362905811fe6f96
DavidCrr <david.contato62@gmail.com>
2025-05-08 18:26:33 -0300

    correção  de metodo get da classe material

--- construcaoCivil/src/model/Material.java
+++ construcaoCivil/src/model/Material.java
@@ -29,7 +29,7 @@ public class Material {
            
         }
         
-        public double precoUnitario(){
+        public double getPrecoUnitario(){
             return precoUnitario;
         }
 
