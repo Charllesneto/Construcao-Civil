40906d82ea9e3c3a60737e6258176254121b1b43
DavidCrr <david.contato62@gmail.com>
2025-05-07 23:44:21 -0300

    criação da classe projetoController
    criação também de um método que busca o cliente por id. creio que irei precisa mais a frente.

---
+++ construcaoCivil/src/controller/ProjetoController.java
@@ -0,0 +1,20 @@
+
+package controller;
+
+import database.Conexao;
+import model.Cliente;
+import model.Projeto;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author David
+ */
+public class ProjetoController {
+    
+  private ClienteController clienteController = new ClienteController(); // para buscar o cliente pelo id.
+    
+}
