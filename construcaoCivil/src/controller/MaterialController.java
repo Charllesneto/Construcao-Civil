f27b33fcd65d9fed3f020b72883e17fac3b51488
DavidCrr <david.contato62@gmail.com>
2025-05-08 20:00:19 -0300

    criação do metodo atualizar material

--- construcaoCivil/src/controller/MaterialController.java
+++ construcaoCivil/src/controller/MaterialController.java
@@ -60,7 +60,69 @@ public class MaterialController {
 
         return lista;
     }
+    // criação do metodo para atualizar os dados de material
+      
+        public boolean atualizarMaterial(Material material) {
+        String sql = "UPDATE material SET nome = ?, unidade_medida = ?, preco_unitario = ? WHERE id_material = ?";
 
-   
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, material.getNome());
+            stmt.setString(2, material.getUnidadeMedida());
+            stmt.setDouble(3, material.getPrecoUnitario());
+            stmt.setInt(4, material.getIdMaterial());
+
+            int linhas = stmt.executeUpdate();
+            return linhas > 0;
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao atualizar material: " + e.getMessage());
+            return false;
+        }
+    }
+        
+        // metodo para remover um material
+        
+    public boolean removerMaterial(int idMaterial) {
+        String sql = "DELETE FROM material WHERE id_material = ?";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, idMaterial);
+            int linhas = stmt.executeUpdate();
+            return linhas > 0;
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao remover material: " + e.getMessage());
+            return false;
+        }
+    }
+    // busca apenas um material pelo id
+     public Material buscarPorId(int idMaterial) {
+        String sql = "SELECT * FROM material WHERE id_material = ?";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, idMaterial);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new Material(
+                    rs.getInt("id_material"),
+                    rs.getString("nome"),
+                    rs.getString("unidade_medida"),
+                    rs.getDouble("preco_unitario")
+                );
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao buscar material por ID: " + e.getMessage());
+        }
+
+        return null;
+    }
     
 }
