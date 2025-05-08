427d4756108ee6a064a3ac0fc462b91fb5c72b7e
DavidCrr <david.contato62@gmail.com>
2025-05-08 12:55:30 -0300

    criação do metodo para adicionar um profissional

--- construcaoCivil/src/controller/ProfissionalController.java
+++ construcaoCivil/src/controller/ProfissionalController.java
@@ -14,4 +14,28 @@ import java.util.List;
 
 public class ProfissionalController {
     
+    // metodo para adicionar um profissional ao banco de dados 
+    
+    public void adicionarProfissional(Profissional profissional) {
+        String sql = "INSERT INTO profissional (id_profissional, nome, cargo, cpf, telefone) VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, profissional.getIdProfissional());
+            stmt.setString(2, profissional.getNome());
+            stmt.setString(3, profissional.getCargo());
+            stmt.setString(4, profissional.getCpf());
+            stmt.setString(5, profissional.get 
+
+            stmt.executeUpdate();
+            System.out.println("Profissional cadastrado com sucesso!");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao cadastrar profissional: " + e.getMessage());
+        }
+    }
+    
+
+    
 }
