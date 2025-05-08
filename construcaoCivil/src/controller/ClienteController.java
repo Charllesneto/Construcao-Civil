2ba4b30c2c9cd035b7e09ce48a71e60515c5c654
DavidCrr <david.contato62@gmail.com>
2025-05-07 21:48:29 -0300

    inicio da modelagem da classe clienteController

--- construcaoCivil/src/controller/ClienteController.java
+++ construcaoCivil/src/controller/ClienteController.java
@@ -1,6 +1,20 @@
 
 package controller;
+/**
+ *
+ * @author david
+ */
+
+import model.Cliente;
+import database.Conexao;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteController {
+    
+public void adicionarCliente(Cliente cliente){
+    String sql = "INSERT INTO cliente (id_cliente, nome, tipo_cliente, cpf_cnpj, telefone) VALUES (?, ?, ?, ?, ?)\";
+}
     
 }
