f53c4ac0b3e2d1d3b3afd3461d14ca5e8e97a802
DavidCrr <david.contato62@gmail.com>
2025-05-07 22:25:03 -0300

    Terminei a criação do metodo adicionarCliente

--- construcaoCivil/src/controller/ClienteController.java
+++ construcaoCivil/src/controller/ClienteController.java
@@ -14,7 +14,21 @@ import java.util.List;
 public class ClienteController {
     
 public void adicionarCliente(Cliente cliente){
-    String sql = "INSERT INTO cliente (id_cliente, nome, tipo_cliente, cpf_cnpj, telefone) VALUES (?, ?, ?, ?, ?)\";
-}
-    
+    String sql = "INSERT INTO cliente (id_cliente, nome, tipo_cliente, cpf_cnpj, telefone) VALUES (?, ?, ?, ?, ?)";
+    try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, cliente.getIdCliente());
+            stmt.setString(2, cliente.getNome());
+            stmt.setString(3, cliente.getTipoCliente());
+            stmt.setString(4, cliente.getCpfCnpj());
+            stmt.setString(5, cliente.getTelefone());
+
+            stmt.executeUpdate();
+            System.out.println("Cliente cadastrado com sucesso!");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao cadastrar cliente: " + e.getMessage());
+        }
+    }
 }
