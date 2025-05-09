8ed121f9641188595647a5c19c3c64a2002a2eac
DavidCrr <david.contato62@gmail.com>
2025-05-09 19:01:42 -0300

    configurações

--- construcaoCivil/src/controller/ProjetoController.java
+++ construcaoCivil/src/controller/ProjetoController.java
@@ -1,4 +1,3 @@
-
 package controller;
 
 import database.Conexao;
@@ -14,23 +13,22 @@ import java.time.LocalDate;
  * @author David
  */
 public class ProjetoController {
-    
-  private ClienteController clienteController = new ClienteController(); // para buscar o cliente pelo id.
-   
-  // Método para adicionar um novo projeto ao BD.
-  
-    public void adicionarProjeto(Projeto projeto) {
-        String sql = "INSERT INTO projeto (id_projeto, nome, descricao, data_inicio, data_fim_prevista, status, id_cliente) " +
-                     "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-        try (Connection conn = Conexao.conectar();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+    private ClienteController clienteController = new ClienteController(); // para buscar o cliente pelo id.
+
+    // Método para adicionar um novo projeto ao BD.
+    public void adicionarProjeto(Projeto projeto) {
+        String sql = "INSERT INTO projeto "
+                + "(id_projeto, nome, descricao, data_inicio, data_fim_prevista, status,fk_cliente_projeto) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = Conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setInt(1, projeto.getIdProjeto());
             stmt.setString(2, projeto.getNome());
             stmt.setString(3, projeto.getDescricao());
-            stmt.setDate(4, Date.valueOf(projeto.getDataInicio()));
-            stmt.setDate(5, Date.valueOf(projeto.getDataFimPrevista()));
+            stmt.setString(4, projeto.getDataInicio());
+            stmt.setString(5, projeto.getDataFimPrevista());
             stmt.setString(6, projeto.getStatus());
             stmt.setInt(7, projeto.getCliente().getIdCliente());
 
@@ -41,28 +39,26 @@ public class ProjetoController {
             System.err.println("Erro ao adicionar projeto: " + e.getMessage());
         }
     }
+
     // metodo que retorna um array com os dados de projeto vindos do BD.
     public List<Projeto> listarProjetos() {
         List<Projeto> projetos = new ArrayList<>();
         String sql = "SELECT * FROM projeto";
-        
+
         // 
-        
-        try (Connection conn = Conexao.conectar();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
-            
+        try (Connection conn = Conexao.conectar(); Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+
             while (rs.next()) {
-                
-                Cliente cliente = clienteController.buscarPorId(rs.getInt("id_cliente"));
+
+                Cliente cliente = clienteController.buscarPorId(rs.getInt("fk_cliente_projeto"));
                 Projeto projeto = new Projeto(
-                    rs.getInt("id_projeto"),
-                    rs.getString("nome"),
-                    rs.getString("descricao"),
-                    rs.getDate("data_inicio").toString(),
-                    rs.getDate("data_fim_prevista").toString(),
-                    rs.getString("status"),
-                    cliente
+                        rs.getInt("id_projeto"),
+                        rs.getString("nome"),
+                        rs.getString("descricao"),
+                        rs.getString("data_inicio"),
+                        rs.getString("data_fim_prevista"),
+                        rs.getString("status"),
+                        cliente
                 );
                 projetos.add(projeto);
             }
@@ -72,17 +68,18 @@ public class ProjetoController {
         }
         return projetos;
     }
+
     // atualiza os dados do projeto e retorna true se alguma linha for alterada
     public boolean atualizarProjeto(Projeto projeto) {
-        String sql = "UPDATE projeto SET nome = ?, descricao = ?, data_inicio = ?, data_fim_prevista = ?, status = ?, id_cliente = ? WHERE id_projeto = ?";
+        String sql = "UPDATE projeto SET nome = ?, descricao = ?, data_inicio = ?, data_fim_prevista = ?, status = ?, fk_cliente_projeto = ? WHERE id_projeto = ?";
 
-        try (Connection conn = Conexao.conectar();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (Connection conn = Conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, projeto.getNome());
             stmt.setString(2, projeto.getDescricao());
-            stmt.setDate(3, Date.valueOf(projeto.getDataInicio()));
-            stmt.setDate(4, Date.valueOf(projeto.getDataFimPrevista()));
+            stmt.setString(3, projeto.getDataInicio());
+            stmt.setString(4, projeto.getDataFimPrevista());
+
             stmt.setString(5, projeto.getStatus());
             stmt.setInt(6, projeto.getCliente().getIdCliente());
             stmt.setInt(7, projeto.getIdProjeto());
@@ -95,12 +92,12 @@ public class ProjetoController {
             return false;
         }
     }
+
     // metodo para deletar o projeto pelo ID.
-     public boolean removerProjeto(int idProjeto) {
+    public boolean removerProjeto(int idProjeto) {
         String sql = "DELETE FROM projeto WHERE id_projeto = ?";
 
-        try (Connection conn = Conexao.conectar();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (Connection conn = Conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setInt(1, idProjeto);
             int linhasAfetadas = stmt.executeUpdate();
@@ -111,27 +108,27 @@ public class ProjetoController {
             return false;
         }
     }
+
     // metodo para fazer uma busca detalhada por id.
     //seleciona o objeto por id e retorna um objeto com os dados do banco
-     public Projeto buscarPorId(int idProjeto) {
+    public Projeto buscarPorId(int idProjeto) {
         String sql = "SELECT * FROM projeto WHERE id_projeto = ?";
 
-        try (Connection conn = Conexao.conectar();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (Connection conn = Conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setInt(1, idProjeto);
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next()) {
-                Cliente cliente = clienteController.buscarPorId(rs.getInt("id_cliente"));
+                Cliente cliente = clienteController.buscarPorId(rs.getInt("fk_cliente_projeto"));
                 return new Projeto(
-                    rs.getInt("id_projeto"),
-                    rs.getString("nome"),
-                    rs.getString("descricao"),
-                    rs.getDate("data_inicio").toString(),
-                    rs.getDate("data_fim_prevista").toString(),
-                    rs.getString("status"),
-                    cliente
+                        rs.getInt("id_projeto"),
+                        rs.getString("nome"),
+                        rs.getString("descricao"),
+                        rs.getString("data_inicio"),
+                        rs.getString("data_fim_prevista"),
+                        rs.getString("status"),
+                        cliente
                 );
             }
 
@@ -140,5 +137,5 @@ public class ProjetoController {
         }
         return null;
     }
-     
+
 }
