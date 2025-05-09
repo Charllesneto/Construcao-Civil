0dc4a7ecd7a6b53af7bb19f542c32290a8417117
DavidCrr <david.contato62@gmail.com>
2025-05-08 21:09:20 -0300

    começo da criação da classe app
    fiz imports de todos as classes do model e controller, import da classe scanner e instanciação da classe scanner, e instancias de cada classe de acesso ao banco.

--- construcaoCivil/src/main/app.java
+++ construcaoCivil/src/main/app.java
@@ -1,7 +1,21 @@
 
 package main;
 
+import controller.*;
+import model.*;
+import java.util.List;
+import java.util.Scanner;
+
 public class app {
+    
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final ClienteController clienteController = new ClienteController();
+    private static final ProfissionalController profissionalController = new ProfissionalController();
+    private static final ProjetoController projetoController = new ProjetoController();
+    private static final EtapaController etapaController = new EtapaController();
+    private static final MaterialController materialController = new MaterialController();
+    private static final UsoMaterialController usoMaterialController = new UsoMaterialController();
+
     
     public static void main(String[] args) {
         
