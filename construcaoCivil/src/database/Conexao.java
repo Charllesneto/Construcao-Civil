4bd4fa17c4236d84cd084eedd1042465dd622020
DavidCrr <david.contato62@gmail.com>
2025-05-09 17:49:07 -0300

    edição da classe

--- construcaoCivil/src/database/Conexao.java
+++ construcaoCivil/src/database/Conexao.java
@@ -11,9 +11,9 @@ import java.sql.SQLException;
 // Na hora de apresentar, a pessoa que for espelhar a tela, deverá atualizar essa área, com seus respectivos dados.
 public class Conexao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/BANCO";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/construcaoCivil";
     private static final String USUARIO = "root";
-    private static final String SENHA = "senha do banco";
+    private static final String SENHA = "Uzumymw6284#";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
