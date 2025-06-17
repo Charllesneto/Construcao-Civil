24f80acf6482654a7f7dbba5c6a8f9f012d41d2f
Charllesnetto <carvalhocharlles4@gmail.com>
2025-06-17 01:17:36 -0300

    Criação da classe tela_aux2
    criei essa classe para selecionar os dados de profissional para adicionar futuramente a classe profissional

---
+++ construcaoCivil/src/Telas_Aux/Tela_aux2.java
@@ -0,0 +1,13 @@
+
+package Telas_Aux;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.*;
+import java.util.List;
+import model.Profissional;
+import controller.ProfissionalController;
+
+public class Tela_aux2 extends JFrame {
+    
+}
